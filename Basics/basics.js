bdf539c17f3b1f908fca89319f5444336e004c61
Abhishek <Abhishekraut8697@gmail.com>
2025-06-12 15:35:43 +0530

    count negative numbers

--- Basics/basics.js
+++ Basics/basics.js
@@ -1,17 +1,28 @@
 //write a function that serches element in the array and return its index and if element is not found then it returns -1
 
 function searchForElement(arr, element) {
-    const len = arr.length;
+  const len = arr.length;
 
-    for (let i = 0; i < len; i++){
-        if (arr[i] === element) {
-            return i;
-        }
+  for (let i = 0; i < len; i++) {
+    if (arr[i] === element) {
+      return i;
     }
-    return -1;
+  }
+  return -1;
 }
 
 //write a code to count negative numbers in array and return it
-function countNegativeNumber(arr){
-    
+function countNegativeNumber(arr) {
+  let count = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < 0) {
+      count++;
+    }
+  }
+
+  return count;
 }
+const arr = [1,3,5,3,6,7,5,2,1,-1,-2]
+const result = countNegativeNumber(arr)
+console.log(result)
