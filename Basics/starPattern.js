15c3c006cb8232aa708e2652308892260a1a5eed
Abhishek <Abhishekraut8697@gmail.com>
2025-06-13 14:32:04 +0530

    full pyramid

--- Basics/starPattern.js
+++ Basics/starPattern.js
@@ -1,13 +1,64 @@
-//printing start pattern
+/*
+printing start pattern
+ *  *  *  * 
+ *  *  *  * 
+ *  *  *  * 
+ *  *  *  * 
+*/
 
 function printSquare(numberOfRows) {
-  let temp = "";
   for (let i = 0; i < numberOfRows; i++) {
+    let temp = "";
     for (let j = 0; j < numberOfRows; j++) {
-      temp += "* ";
+      temp += " * ";
     }
-    console.log(temp)
-    temp = ""
+    console.log(temp);
   }
 }
-printSquare(4)
+
+/*
+printing start pattern
+
+* 
+* * 
+* * * 
+* * * * 
+
+*/
+
+function printHalfPyramid(numberOfRows) {
+  for (let i = 0; i < numberOfRows; i++) {
+    let temp = "";
+    for (let j = 0; j < i; j++) {
+      temp += "* ";
+    }
+    console.log(temp);
+  }
+}
+
+/*
+printing start pattern
+
+   *        numberOfRows 4 - i -1
+  * *       numberOfRows 4 - 1 -1
+ * * *      numberOfRows 4 - 2 -1
+* * * * 
+
+*/
+
+function printFullPyramid(numberOfRows) {
+  for (let i = 0; i < numberOfRows; i++) {
+    let temp = "";
+    for (let j = 0; j < numberOfRows - i - 1; j++) {
+      temp += " ";
+    }
+
+    for (let j = 0; j < i + 1; j++) {
+      temp += " *";
+    }
+
+    console.log(temp);
+  }
+}
+
+printFullPyramid(5);
