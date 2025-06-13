a5010947b539b34c12f132f8dc9359394759aa79
Abhishek <Abhishekraut8697@gmail.com>
2025-06-13 16:32:19 +0530

    easy problem mast solve ho jate hai

--- Basics/CountDigits.js
+++ Basics/CountDigits.js
@@ -1,11 +1,35 @@
 //write a function that count a number of digits in number
 
-function countDigit(num){
+function JugaduCountDigit(num){
     let count = 0;
-
-
+    const temp = new String(num)
+    count = temp.length
     return count
 }
 
-const result = countDigit(3141323413)
+function JugaduCountDigit2(num){
+    let count = 0;
+    const temp = Math.abs(num).toString().split('')
+    count = temp.length
+    return count
+}
+
+function CountDigit(num){
+    /*
+        while(num>0){
+        464 % 10  reminder = 4
+        46 % 10   remainder = 6
+        4 % 10   remainder = 4
+        }
+    */
+   let count = 0
+   let temp = num
+    while(temp > 0){
+        temp = Math.floor(temp/10)
+        count++;
+    }
+    return count
+}
+
+const result = CountDigit(749323413)
 console.log(result)
