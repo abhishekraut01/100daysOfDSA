04153d76655ef9cc42caace7902367727f62cf9a
Abhishek <Abhishekraut8697@gmail.com>
2025-06-18 01:23:27 +0530

    DSAAAAAAA

--- DSA_IN_JS/removeDuplicates.ts
+++ DSA_IN_JS/removeDuplicates.ts
@@ -29,3 +29,16 @@ function removeDuplicates(nums: number[]): number {
 };
 
 //this is more optimized way and have O(n) time complexity
+
+
+function removeDuplicatesOptimized(nums: number[]): number {
+    //and here is one of the most optimized approach two pointerrrrrr 
+    let x = 0;
+    for(let i=0; i<nums.length; i++){
+        if(nums[i] > nums[x]){
+            x++
+            nums[x] = nums[i]
+        }
+    }
+    return x+1;
+};
